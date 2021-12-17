4d16c4920a0dd7aa4d53891e0189a4cf1aad6d72
Litvinka3007 <angelina16161616@gmail.com>
2021-12-17 16:17:18 +0300

    add a custom navigation for mobile, tablet and desctop screen sizes

--- frontend/Singree/tech-store/Magento_Theme/web/js/search-field.js
+++ frontend/Singree/tech-store/Magento_Theme/web/js/search-field.js
@@ -1,19 +1,21 @@
 define([], function(){
     return function () {
-        let loup = document.querySelector('#search-icon'),
-            searchField = document.querySelector('.block-search'),
+        let searchField = document.querySelector('.block-search'),
+            loup = document.getElementById('search-icon'),
+            fieldSearch = document.getElementById('search'),
             search = 'loup',
             replaceWith = 'close',
-            imageSrc = document.getElementById("loup-icon").getAttribute('src').split(replaceWith).join(search),
-            imageSrc2 = document.getElementById("loup-icon").getAttribute('src').split(search).join(replaceWith)
+            imageSrc = document.getElementById('loup-icon').getAttribute('src').split(replaceWith).join(search),
+            imageSrc2 = document.getElementById('loup-icon').getAttribute('src').split(search).join(replaceWith)
 
         loup.addEventListener('click', () => {
             searchField.classList.toggle('visible');
+            fieldSearch.focus();
 
             if (searchField.classList.contains('visible')) {
-                document.getElementById("loup-icon").src = imageSrc2;
+                document.getElementById('loup-icon').src = imageSrc2;
             } else {
-                document.getElementById("loup-icon").src = imageSrc;
+                document.getElementById('loup-icon').src = imageSrc;
             }
         })
     }
