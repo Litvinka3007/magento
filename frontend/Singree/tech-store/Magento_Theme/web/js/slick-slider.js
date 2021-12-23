16208020d5818015ecd6f5035da778aefd1b97f2
Litvinka3007 <angelina16161616@gmail.com>
2021-12-23 18:06:42 +0300

    add testimonials section to the project and fix the alignment of the benefits-block

--- frontend/Singree/tech-store/Magento_Theme/web/js/slick-slider.js
+++ frontend/Singree/tech-store/Magento_Theme/web/js/slick-slider.js
@@ -4,34 +4,17 @@ require([
 ], function ($) {
     jQuery(document).ready(function () {
         jQuery(".slider-slick").slick({
+            arrows: false,
             dots: true,
             infinite: true,
             speed: 300,
-            slidesToShow: 4,
-            slidesToScroll: 4,
-            responsive: [
-                {
-                    breakpoint: 1280,
-                    settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 3
-                    }
-                },
-                {
-                    breakpoint: 768,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 2
-                    }
-                },
-                {
-                    breakpoint: 600,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1
-                    }
-                }
-            ]
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            initialSlide: 0,
+            draggable: true,
+            swipe: true,
+            touchThreshold: 10,
+            touchMove: true
         });
     });
 });
--- frontend/Singree/tech-store/requirejs-config.js
+++ frontend/Singree/tech-store/requirejs-config.js
@@ -2,6 +2,7 @@ var config = {
     map: {
         '*': {
             slick: 'Magento_Theme/js/slick.min'
+            // slider: 'Magento_Theme/js/slick-slider'
         }
     },
 
